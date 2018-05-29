386156f0fb69d2c77035e69d82e8b40a3f51b149
bob banana <bob banana>
2018-05-29 14:44:27 -0400

    Progress2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'callbacks/omniauth_auth'
 
-  devise_for :users
+  get 'callbacks/omniauth_authorize', to: 'callbacks#omniauth_auth'
+  get '/home', to: 'pages#home'
+
+  devise_for :users, :controllers => {:omniauth_callbacks => "callbacks"}
+
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
